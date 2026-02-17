2d234a097369aef826f5e7198e568c69c69e0a0f
Konstantin <bormotovk@gmail.com>
2026-02-17 09:17:51 +0100

    redirect

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://apidev.alor.ru',
   userDataUrl: 'https://lk-api-dev.alorbroker.ru',
   ssoUrl: 'https://login-dev.alor.ru',
-  superAppUrl: 'https://maxminiapp-dev.alor.ru/superapp'
+  superAppUrl: 'https://maxminiapp.dev.alor.ru/superapp'
 };
