7be59fe9b0a9e499166598b4609e7a401ac8a54a
Konstantin <bormotovk@gmail.com>
2026-03-24 00:52:43 +0100

    Revert "Buttons"
    This reverts commit 37bf8d7b89de1681814f6c14f405f26401e12525.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -8,23 +8,23 @@ export const IconLogout = ({ width = 24, height = 24, className }: { width?: num
 
 export const IconNewOrder = ({ width = 24, height = 24, className }: { width?: number | string; height?: number | string; className?: string }) => (
   <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-    <path d="M12 4V20M4 12H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 4V20M4 12H20" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
 export const IconCancelAll = ({ width = 24, height = 24, className }: { width?: number | string; height?: number | string; className?: string }) => (
   <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-    <path d="M18.36 6.64L6.64 18.36" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M6.64 6.64L18.36 18.36" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M18.36 6.64L6.64 18.36" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M6.64 6.64L18.36 18.36" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
 export const IconOperationsHistory = ({ width = 24, height = 24, className }: { width?: number | string; height?: number | string; className?: string }) => (
   <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-    <path d="M3 6.75C3 5.7835 3.7835 5 4.75 5H19.25C20.2165 5 21 5.7835 21 6.75V17.25C21 18.2165 20.2165 19 19.25 19H4.75C3.7835 19 3 18.2165 3 17.25V6.75Z" stroke="currentColor" strokeWidth="2" />
-    <path d="M7 12H17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-    <path d="M7.5 9L6 10.5L7.5 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M16.5 12L18 13.5L16.5 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M3 6.75C3 5.7835 3.7835 5 4.75 5H19.25C20.2165 5 21 5.7835 21 6.75V17.25C21 18.2165 20.2165 19 19.25 19H4.75C3.7835 19 3 18.2165 3 17.25V6.75Z" stroke="white" strokeWidth="2" />
+    <path d="M7 12H17" stroke="white" strokeWidth="2" strokeLinecap="round" />
+    <path d="M7.5 9L6 10.5L7.5 12" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M16.5 12L18 13.5L16.5 15" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
